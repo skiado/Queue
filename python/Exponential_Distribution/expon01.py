da27c45e4c188d48ed9da37af9b6754e51c32fa8
skiado <70379186+skiado@users.noreply.github.com>
2024-02-18 19:16:45 +0200

    Update expon01.py

--- python/Exponential_Distribution/expon01.py
+++ python/Exponential_Distribution/expon01.py
@@ -1,1 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt  #Για τη δημιουργία γραφικών παραστάσεων
+import scipy as sp
 
+rng = np.random.default_rng()
+
+def exponential():
+    U = rng.random()
+    return -np.log(U)
+ 
+MAX = 100000
+a = []
+l = float(input('l = '))
+for i in range(MAX):
+    a.append(l*exponential())
+b = rng.exponential(l,MAX)
+print('From numpy library ')
+print('mean E[X] = ',np.mean(b))
+print('From my function')
+print('mean E[X] = ',np.mean(a))
