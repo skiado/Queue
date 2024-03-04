e04325d51266c8970d10d41f4e9081261e318c41
skiado <70379186+skiado@users.noreply.github.com>
2024-03-04 17:58:45 +0200

    Update expon01.py

--- python/Exponential_Distribution/expon01.py
+++ python/Exponential_Distribution/expon01.py
@@ -1,6 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt  #Για τη δημιουργία γραφικών παραστάσεων
-import scipy as sp
+import matplotlib.pyplot as plt  # Για την περίπτωση που θέλουμε να δημιουργήσουμε γραφική παράσταση
 
 rng = np.random.default_rng()
 
@@ -10,10 +9,12 @@ def exponential():
  
 MAX = 100000
 a = []
-l = float(input('l = '))
+l = float(input('λ = '))
 for i in range(MAX):
-    a.append(l*exponential())
-b = rng.exponential(l,MAX)
+    a.append((1/l)*exponential())
+b = rng.exponential((1/l),MAX)
+print('From Theory')
+print('mean E[X] = ',1/l)
 print('From numpy library ')
 print('mean E[X] = ',np.mean(b))
 print('From my function')
